d2b4e1991c36c79e9701b7b57b1984cdd26a4b49
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2021-10-06 20:35:41 -0300

    modificaciones de estilo lista de empleados

--- html/ListaDeEmpleados.php
+++ html/ListaDeEmpleados.php
@@ -7,13 +7,71 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="media.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href='https://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
+
     <title></title>
 </head>
+
 <body>
-    
+
+    <div class="listaEmpleado">
+        <nav class="navbar">
+            <div class="content">
+                <div class="logo">
+                    <a href="#">San Pedro</a>
+                </div>
+                <ul class="menu-list">
+                    <div class="icon cancel-btn">
+                        <i class="fas fa-times"></i>
+                    </div>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Staff</a></li>
+                    <li><a href="#">Supplier</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Register sale</a></li>
+                    <li><a href="#">Statistics</a></li>
+                </ul>
+                <div class="icon menu-btn">
+                    <i class="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
+
+
+
+
+        <div class="Divcontainer">
+            <h2>Lista de empleados:</h2>
+            <table>
+                <tr>
+                    <th>NOMBRE</th>
+                    <th>APELLIDO</th>
+                    <th>DNI</th>
+                    <th>ADELANTOS</th>
+                </tr>
+
+                <tr>
+                    <td>nombre</td>
+                    <td>apellido</td>
+                    <td>DNI</td>
+                    <td>adelantos</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
+
+
+    <script src="js/app.js"></script>
 </body>
+
 </html>
--- html/home.php
+++ html/home.php
@@ -36,12 +36,6 @@
         </div>
     </nav>
 
-
-    <div class="container">
-
-
-    </div>
-
     <div class="content">
         <div class="txt-home">
             <h1>Hi @nombre</h1>
@@ -62,27 +56,7 @@
         </p>
     </footer>
 
-
-    <script>
-        const body = document.querySelector("body");
-        const navbar = document.querySelector(".navbar");
-        const menuBtn = document.querySelector(".menu-btn");
-        const cancelBtn = document.querySelector(".cancel-btn");
-        menuBtn.onclick = () => {
-            navbar.classList.add("show");
-            menuBtn.classList.add("hide");
-            body.classList.add("disabled");
-        }
-        cancelBtn.onclick = () => {
-            body.classList.remove("disabled");
-            navbar.classList.remove("show");
-            menuBtn.classList.remove("hide");
-        }
-        window.onscroll = () => {
-            this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-        }
-    </script>
-
+    <script src="js/app.js"></script>
 </body>
 
 </html>
