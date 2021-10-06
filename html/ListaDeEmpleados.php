c31b28a854e5e445d4cdb7e21fea5f6ef8a6cb2b
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2021-10-06 17:18:47 -0300

    mejoras de diseño

--- html/ListaDeEmpleados.php
+++ html/ListaDeEmpleados.php
@@ -6,11 +6,14 @@
 <!-- Y supongo que otro boton para poder ver los adelantos que se hicieron (me tiene que llevar a otra pantalla) -->
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
 </head>
 <body>
-
+    
 </body>
 </html>
